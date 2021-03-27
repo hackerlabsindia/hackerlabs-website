6d9b39dfa9eb483b105740700e9ae43e12c12eb2
bim240 <bimlendu357@gmail.com>
2021-03-27 22:00:52 +0530

    header improve

--- src/Components/Header/StyledComonents.js
+++ src/Components/Header/StyledComonents.js
@@ -4,4 +4,45 @@ export const HeaderWrapper = styled.div`
   height: 4rem;
   background: ${(props) => props.theme.colors.secondaryBackgroundColor};
   width: 100%;
+  color: white;
+  .container {
+    width: 85%;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .logo_section {
+      height: 100%;
+      display: flex;
+      align-items: center;
+      .title {
+        padding: 0;
+        margin: 0;
+      }
+    }
+    img {
+      width: 3rem;
+      margin-right: 0.5rem;
+    }
+
+    nav {
+      height: 100%;
+      display: flex;
+      align-items: center;
+      a {
+        padding: 0.5rem 1rem;
+        text-decoration: none;
+        color: white;
+        font-weight: 700;
+        border-radius: 4px;
+      }
+      a:hover {
+        outline: 1px solid white;
+      }
+      active {
+        outline: 1px solid white;
+      }
+    }
+  }
 `;
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,7 +1,23 @@
 import { HeaderWrapper } from "./StyledComonents";
 
 const Header = (props) => {
-  return <HeaderWrapper> </HeaderWrapper>;
+  return (
+    <HeaderWrapper>
+      <div className="container">
+        <div className="logo_section">
+          <img src="./logo192.png" alt="logo" />
+          <h1 className="title"> Hacker Labs</h1>
+        </div>
+        <nav>
+          <a href="#hero">Home</a>
+          <a href="#developer">Developer</a>
+          <a href="#techstach">Tech Stack</a>
+
+          <a href="#contactus">Contact Us</a>
+        </nav>
+      </div>
+    </HeaderWrapper>
+  );
 };
 
 export default Header;
